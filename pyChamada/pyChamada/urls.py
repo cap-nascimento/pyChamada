2ea94dabd764ae825518d4871922f0d0c818ec36
cap-nascimento <anascimentovieira2@gmail.com>
2023-05-16 20:40:42 -0300

    - Deleta apps (Aluno e Professor); - Cria apps login e user.

--- pyChamada/pyChamada/urls.py
+++ pyChamada/pyChamada/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('aluno/', include('aluno.urls')),
+    path('login/', include('login.urls')),
     path('admin/', admin.site.urls),
 ]
