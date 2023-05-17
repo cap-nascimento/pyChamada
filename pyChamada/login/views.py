4d7d86c2950fb08da4c6be1f6402e9ea35f06e2e
cap-nascimento <anascimentovieira2@gmail.com>
2023-05-17 16:51:42 -0300

    adding some html to login page

--- pyChamada/login/views.py
+++ pyChamada/login/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Tela de login")
+    return HttpResponse(render(request, 'login/index.html'))
--- pyChamada/user/models.py
+++ pyChamada/user/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    nome = models.CharField(max_length=200)
+    matricula = models.CharField(max_length=200)
+    cpf = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
