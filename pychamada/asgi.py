c30648ad9eb113f9318e7f51bbc54621117a8c5b
cap-nascimento <anascimentovieira2@gmail.com>
2023-05-19 15:46:27 -0300

    Create fresh project closes #1 - Built-in login system

--- pychamada/asgi.py
+++ pychamada/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for pyChamada project.
+ASGI config for pychamada project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pyChamada.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pychamada.settings')
 
 application = get_asgi_application()
--- pychamada/urls.py
+++ pychamada/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for pyChamada project.
+URL configuration for pychamada project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('login/', include('login.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
