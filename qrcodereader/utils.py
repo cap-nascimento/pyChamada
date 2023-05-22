2980ca17e7e4697fd55c834d3a9ddecb6371d85b
cap-nascimento <anascimentovieira2@gmail.com>
2023-05-22 09:25:20 -0300

    Criando view para registro de presença - O registro de presença está sendo feito da seguinte forma: 1. O usuário solicita o QRCode; 2. O QRCode é gerado com um token que consiste da data e hora em que o QR foi solicitado e o id da aula. O token é criptografado (utils.py); 3. Ao acessar a URL com o token, o token é descriptografado com a hora, data e id de aula; 4. A hora e data é comparada com a hora e data em que o link do QRCode é acessado; 5. Se a diferença entre as datas for menor ou igual a 300 segundos (5 minutos), a presença é registrada, caso contrário, não é registrada.

--- qrcodereader/utils.py
+++ qrcodereader/utils.py
@@ -1,3 +1,11 @@
+from Crypto.Protocol.KDF import PBKDF2
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+from Crypto.Hash import SHA256
+from Crypto.Util.Padding import pad
+from Crypto.Util.Padding import unpad
+from datetime import datetime
+import base64
 import qrcode
 
 
@@ -6,7 +14,7 @@ def generate_qrcode(data):
     for key in data.keys():
         content += data[key]
         
-    qr = qrcode.QRCode(version=1, box_size=15)
+    qr = qrcode.QRCode(version=1, box_size=10)
     qr.add_data(content)
     
     filename = 'tempqrcode.png'
@@ -14,3 +22,57 @@ def generate_qrcode(data):
     img.save('qrcodereader/static/images/' + filename)
     
     return filename
+
+
+def base64Encoding(input):
+  dataBase64 = base64.b64encode(input)
+  dataBase64P = dataBase64.decode("UTF-8")
+  return dataBase64P
+
+
+def base64Decoding(input):
+    return base64.decodebytes(input.encode("ascii"))
+
+
+def encrypt_content(message, key):
+    keyBytes = key.encode('ascii')
+    salt = get_random_bytes(32)
+    encryptionKey = PBKDF2(keyBytes, salt, 32, count=15000, hmac_hash_module=SHA256)
+    cipher = AES.new(encryptionKey, AES.MODE_CBC)
+    ciphertext = cipher.encrypt(pad(message.encode('ascii'), AES.block_size))
+    ivBase64 = base64Encoding(cipher.iv)
+    saltBase64 = base64Encoding(salt)
+    ciphertextBase64 = base64Encoding(ciphertext)
+    return saltBase64 + ":" + ivBase64 + ":" + ciphertextBase64
+
+
+def decrypt_content(message, key):
+    keyBytes = key.encode('ascii')
+    data = message.split(':')
+    salt = base64Decoding(data[0])
+    iv = base64Decoding(data[1])
+    ciphertext = base64Decoding(data[2])
+    decryptionKey = PBKDF2(keyBytes, salt, 32, 15000, hmac_hash_module=SHA256)
+    cipher = AES.new(decryptionKey, AES.MODE_CBC, iv)
+    decryptedText = unpad(cipher.decrypt(ciphertext), AES.block_size)
+    decryptedTextP = decryptedText.decode('UTF-8')
+    return decryptedTextP
+
+
+def format_datetime(input):
+    input_formatted_str = input.strftime("%d-%m-%y %H:%M:%S")
+    return datetime.strptime(input_formatted_str, "%d-%m-%y %H:%M:%S")
+
+
+def timecount(input):
+    input = input.replace(' ', '')
+    result = 0
+    if input.find('day') != -1:
+        input = input.replace('days', '').replace('day', '')
+        days, hours = input.split(',')
+        result += int(days) * 24*60*60
+        input = hours
+    
+    hour, min, sec = input.split(':')
+    result += int(hour)*60*60 + int(min)*60 + int(sec)
+    return result
--- turma/urls.py
+++ turma/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('create/turma/post/', views.create_turma_post, name='create_turma_post'),
     path('detail/<int:turma_id>/', views.detail_turma, name='detail_turma'),
     path('<int:turma_id>/create/aula/', create_aula, name='create_aula'),
-    path('<int:turma_id>/create/aula/post', create_aula_post, name='create_aula_post'),
-    path('<int:turma_id>/register', views.register_aluno, name='register_aluno')
-
+    path('<int:turma_id>/create/aula/post/', create_aula_post, name='create_aula_post'),
+    path('<int:turma_id>/register/', views.register_aluno, name='register_aluno'),
+    path('aula/<path:token>/', views.register_presenca, name='register_presenca'),
 ]
